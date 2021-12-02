b814e7943f0c4d64d0226db5b9c2d300174f5b4e
Gabriel Gomes <dev.gabrielsoares221@gmail.com>
2021-12-02 10:20:05 -0300

    Adicionando projeto inteiro

--- main.js
+++ main.js
@@ -1,2 +1,51 @@
-posicaoAtual = document.querySelectorAll('#quadrado00').value;
-console.log(posicaoAtual)
+function moverPersonagem(direcao) {
+    let posicaoAtual = document.getElementsByClassName('active')[0];
+    let novaPosicao = parseInt(posicaoAtual.innerHTML)
+    switch (direcao) {
+        case "cima":
+            novaPosicao -= 10
+            if (novaPosicao < 0 || novaPosicao > 49) {
+                alert("Que feio, querendo sair do mapa!")
+            } else {
+                posicaoAtual.classList.remove('active')
+                document.getElementById("quadrado" + novaPosicao).classList.add("active")
+            }
+            break
+        case "esquerda":
+            novaPosicao -= 1
+            if (novaPosicao < 0 || novaPosicao > 49) {
+                alert("Que feio, querendo sair do mapa!")
+            } else {
+                posicaoAtual.classList.remove('active')
+                document.getElementById("quadrado" + novaPosicao).classList.add("active")
+            }
+            break
+        case "baixo":
+            novaPosicao += 10
+            if (novaPosicao < 0 || novaPosicao > 49) {
+                alert("Que feio, querendo sair do mapa!")
+            } else {
+                posicaoAtual.classList.remove('active')
+                document.getElementById("quadrado" + novaPosicao).classList.add("active")
+            }
+            break
+        case "direita":
+            novaPosicao += 1
+            if (novaPosicao < 0 || novaPosicao > 49) {
+                alert("Que feio, querendo sair do mapa!")
+            } else {
+                posicaoAtual.classList.remove('active')
+                document.getElementById("quadrado" + novaPosicao).classList.add("active")
+            }
+            break
+    }
+
+}
+
+// document.getElementById("MyElement").classList.add('MyClass');
+//
+// document.getElementById("MyElement").classList.remove('MyClass');
+//
+// if ( document.getElementById("MyElement").classList.contains('MyClass') )
+//
+// document.getElementById("MyElement").classList.toggle('MyClass');
